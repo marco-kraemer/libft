55c1f6b4e93638592f0679cbbeef689ad87b9d9e
marco-kraemer <marco.k.santos@hotmail.com>
2021-02-08 17:39:07 +0100

    Starting bonus

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/07 22:15:25 by maraurel          #+#    #+#             */
+/*   Updated: 2021/02/08 12:35:30 by maraurel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*p;
+	int		i;
+
+	i = 0;
+	p = (char *)malloc(ft_strlen(s) + 1);
+	if (p == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		p[i] = f(i, s[i]);
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
--- libft.h
+++ libft.h
@@ -3,50 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maraurel <maraurel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/03 12:10:07 by maraurel          #+#    #+#             */
-/*   Updated: 2021/02/03 12:10:07 by maraurel         ###   ########.fr       */
+/*   Created: 2021/02/08 13:32:19 by maraurel          #+#    #+#             */
+/*   Updated: 2021/02/08 15:24:25 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef libft
-#define libft
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef LIBFT_H
+# define LIBFT_H
 
-void    *ft_memset(void *s, int c, size_t n);
-void    ft_bzero(void *s, size_t n);
-void    *ft_memcpy(void *dest, const void * src, size_t n);
-void    *ft_memccpy(void *dest, const void *src, int c, size_t n);
-void    *ft_memmove(void *dest, const void *src, size_t n);
-void    *ft_memchr(const void *str, int c, size_t n);
-void    *ft_calloc(size_t nmemb, size_t size);
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int     ft_atoi(const char *nptr);
-int     ft_memcmp(const void *s1, const void *s2, size_t n);
-int     ft_isalpha(int c);
-int     ft_isdigit(int c);
-int     ft_isalnum(int c);
-int     ft_isascii(int c);
-int     ft_isprint(int c);
-int     ft_toupper(int c);
-int     ft_tolower(int c);
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
-size_t  ft_strlen(const char *str);
-size_t  ft_strlcpy(char *dst, const char *src, size_t size);
-size_t  ft_strlcat(char *dst, const char *src, size_t size);
-
-char    *ft_strchr(const char *str, int c);
-char    *ft_strrchr(const char *s, int c);
-char    *ft_strnstr(const char *big, const char *little, size_t len);
-char    *ft_strdup(const char *s);
-char    *ft_substr(char const *s, unsigned int start, size_t len);
-char    *ft_strjoin(char const *s1, char const *s2);
-char    *ft_strtrim(char const *s1, char const *set);
-char    *ft_itoa(int n);
-
-char    **ft_split(char const *s, char c);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putstr_fd(char *s, int fd);
+void				ft_putendl_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
+void				*ft_memset(void *s, int c, size_t n);
+void				ft_bzero(void *s, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void				*ft_memmove(void *dest, const void *src, size_t n);
+void				*ft_memchr(const void *str, int c, size_t n);
+void				*ft_calloc(size_t nmemb, size_t size);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_atoi(const char *nptr);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+int					ft_lstsize(t_list *lst);
+size_t				ft_strlen(const char *str);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
+size_t				ft_strlcat(char *dst, const char *src, size_t size);
+char				*ft_strchr(const char *str, int c);
+char				*ft_strrchr(const char *s, int c);
+char				*ft_strnstr(const char *big, const char *little,
+		size_t len);
+char				*ft_strdup(const char *s);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strtrim(char const *s1, char const *set);
+char				*ft_itoa(int n);
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char				**ft_split(char const *s, char c);
+t_list				*ft_lstnew(void *content);
+t_list				*ft_lstlast(t_list *lst);
 
 #endif
