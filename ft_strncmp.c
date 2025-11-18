1f9d8e35308036927693cbb8f9cc925e0e133ce4
Marco Santos <msantos2@c4a12c5.42berlin.de>
2025-11-18 13:48:20 +0100

    Libft

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,25 @@
 /*   By: msantos2 <msantos2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 13:33:03 by msantos2          #+#    #+#             */
-/*   Updated: 2025/11/18 13:33:04 by msantos2         ###   ########.fr       */
+/*   Updated: 2025/11/18 13:38:31 by msantos2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+	int				ret;
+
+	i = 0;
+	ret = 0;
+	while ((s1[i] != 0 || s2[i] != 0) && i < n)
+	{
+		if (s1[i] != s2[i])
+		{
+			ret = ret - (s2[i] - s1[i]);
+			return (ret);
+		}
+		i++;
+	}
+	return (ret);
+}
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,47 @@
 /*   By: msantos2 <msantos2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 13:33:01 by msantos2          #+#    #+#             */
-/*   Updated: 2025/11/18 13:33:01 by msantos2         ###   ########.fr       */
+/*   Updated: 2025/11/18 13:39:08 by msantos2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strnstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+	int	pos;
+
+	i = 0;
+	if (ft_strlen(to_find) == 0)
+		return (&str[0]);
+	while (str[i])
+	{
+		j = 0;
+		pos = 0;
+		if (str[i] == to_find[j])
+		{
+			pos = i;
+			while (str[i] && str[i] == to_find[j])
+			{
+				i++;
+				j++;
+			}
+			if (j == ft_strlen(to_find))
+				return (&str[pos]);
+		}
+		i++;
+	}
+	return (NULL);
+}
