73738e6ce6ae46e5da349141dea3e8d0a0379308
Marco Santos <msantos2@c3a10c6.42berlin.de>
2025-11-23 14:46:28 +0100

    Libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: msantos2 <msantos2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:01:32 by msantos2          #+#    #+#             */
-/*   Updated: 2025/11/22 15:01:31 by msantos2         ###   ########.fr       */
+/*   Updated: 2025/11/23 14:45:06 by msantos2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@
 void	*ft_calloc(size_t nitems, size_t size)
 {
 	void	*alloc;
+	size_t	len;
 
-	alloc = malloc (nitems * size);
+	len = nitems * size;
+	if (len != 0 && size != len / nitems)
+		return (NULL);
+	alloc = malloc(len);
 	if (!alloc)
 		return (NULL);
-	return (ft_memset(alloc, 0, nitems * size));
+	return (ft_memset(alloc, 0, len));
 }
